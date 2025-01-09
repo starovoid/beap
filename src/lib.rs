4380b5531d560415d145235fe6f6d1886312628f
starovoid <prototyperailgun@gmail.com>
2025-01-09 20:54:09 +0300

    Add index, contains & get_mut to the example

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,18 @@ use std::ops::{Deref, DerefMut};
 /// // We can check the length of a beap.
 /// assert_eq!(beap.len(), 3);
 ///
+/// // You can check if an item is contained in the beap.
+/// assert!(beap.contains(&2));
+///
+/// // Or get mutable access to it:
+/// let idx = beap.index(&2).unwrap();
+/// {
+///     let mut val = beap.get_mut(idx).unwrap();
+///     assert_eq!(*val, 2);
+///     *val = 20;
+/// }
+/// assert_eq!(beap.peek(), Some(&20));
+///
 /// // We can iterate over the items in the beap, although they are returned in
 /// // a random order.
 /// for x in beap.iter() {
@@ -81,8 +93,8 @@ use std::ops::{Deref, DerefMut};
 /// }
 ///
 /// // If we instead pop these scores, they should come back in order.
+/// assert_eq!(beap.pop(), Some(20));
 /// assert_eq!(beap.pop(), Some(5));
-/// assert_eq!(beap.pop(), Some(2));
 /// assert_eq!(beap.pop(), Some(1));
 /// assert_eq!(beap.pop(), None);
 ///
